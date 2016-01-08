214d80abe30cf4058268011f10850c7563a17be4
unknown <>
2016-01-08 23:19:54 +0800

    完成django的本地搭建与测试

--- learn_django/learn_django/settings.py
+++ learn_django/learn_django/settings.py
@@ -84,7 +84,7 @@ DATABASES = {
         'USER'          : 'frankstar',
         'PASSWORD'      : 'frankstar',
         'HOST'          : '127.0.0.1',
-        'PORT'          : '5433',
+        'PORT'          : '5432',
     }
 }
 
