91895ad29312e05ff1ffe75fd21c5bb1cd0059da
frankstar007 <568106896@qq.com>
2016-01-10 13:34:19 +0800

    路由与视图的整理

--- learn_django/polls/views.py
+++ learn_django/polls/views.py
@@ -1,6 +1,32 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.template import loader
+from django.http import Http404
+from django.shortcuts import render,get_object_or_404
+from .models import Question
 
 def index(request):
-    return HttpResponse('hello frankstar! welcome to django!')
+    latest_question_list = Question.objects.order_by('pub_date')[:5]
+    #output = ', '.join([q.question_text for q in latest_question_list])
+    template = loader.get_template('polls/index.html')
+    context = {
+        'latest_question_list' : latest_question_list
+    }
+    return HttpResponse(template.render(context, request))
 # Create your views here.
+def detail(request, question_id):
+    # try:
+    #     question = Question.objects.get(pk=question_id)
+    # except Question.DoesNotExist:
+    #     raise Http404('Question does not exist!')
+    question = get_object_or_404(Question, pk=question_id)
+
+    return render(request, 'polls/detail.html', {'question':question})
+
+def results(request, question_id):
+
+    return HttpResponse("you're looking at the results of question %s." % question_id)
+
+def vote(request, question_id):
+
+    return HttpResponse("you're voting the question %s." % question_id)
